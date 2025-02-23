fa8c47dbe6d8b78d2877e4802e6485a79d6c5dd2
Anthony DeMattos <demattosanthony@gmail.com>
2025-02-23 15:10:08 -0500

    Refactor SQL query to remove file type prefixes for improved readability

--- api/drizzle/0028_freezing_firelord.sql
+++ api/drizzle/0028_freezing_firelord.sql
@@ -10,12 +10,12 @@ WHERE d.file_key IS NOT NULL
   AND dpj.document_id IS NULL
   AND (
     LOWER(SUBSTRING(d.name FROM '\.([^\.]+)$')) IN (
-      '.abw', '.bmp', '.csv', '.cwk', '.dbf', '.dif', '.doc', '.docm', '.docx',
-      '.dot', '.dotm', '.eml', '.epub', '.et', '.eth', '.fods', '.gif', '.heic',
-      '.htm', '.html', '.hwp', '.jpeg', '.jpg', '.md', '.mcw', '.mw', '.odt',
-      '.org', '.p7s', '.pages', '.pbd', '.pdf', '.png', '.pot', '.potm', '.ppt',
-      '.pptm', '.pptx', '.prn', '.rst', '.rtf', '.sdp', '.sgl', '.svg', '.sxg',
-      '.tiff', '.txt', '.tsv', '.uof', '.uos1', '.uos2', '.web', '.webp', '.wk2',
-      '.xls', '.xlsb', '.xlsm', '.xlsx', '.xlw', '.xml', '.zabw'
+      'abw', 'bmp', 'csv', 'cwk', 'dbf', 'dif', 'doc', 'docm', 'docx',
+      'dot', 'dotm', 'eml', 'epub', 'et', 'eth', 'fods', 'gif', 'heic',
+      'htm', 'html', 'hwp', 'jpeg', 'jpg', 'md', 'mcw', 'mw', 'odt',
+      'org', 'p7s', 'pages', 'pbd', 'pdf', 'png', 'pot', 'potm', 'ppt',
+      'pptm', 'pptx', 'prn', 'rst', 'rtf', 'sdp', 'sgl', 'svg', 'sxg',
+      'tiff', 'txt', 'tsv', 'uof', 'uos1', 'uos2', 'web', 'webp', 'wk2',
+      'xls', 'xlsb', 'xlsm', 'xlsx', 'xlw', 'xml', 'zabw'
     )
   );
