57b03ccbe289318249a637466449f40dc4c1631c
Anthony DeMattos <demattosanthony@gmail.com>
2025-04-21 14:12:52 -0400

    undo migrations

--- api/drizzle/0042_shiny_morg.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "document_embeddings" ADD COLUMN "contextual_summary" text;--> statement-breakpoint
-ALTER TABLE "document_embeddings" ADD COLUMN "type" text;--> statement-breakpoint
-ALTER TABLE "document_embeddings" ADD COLUMN "image_file_key" text;
