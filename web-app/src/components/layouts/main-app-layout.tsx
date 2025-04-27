7eca76d627c3855678f4e2d8c9ce464a0a07fd32
Anthony DeMattos <demattosanthony@gmail.com>
2025-04-26 20:23:38 -0400

    fixed overflow scroll

--- web-app/src/components/layouts/main-app-layout.tsx
+++ web-app/src/components/layouts/main-app-layout.tsx
@@ -9,18 +9,16 @@ export default function MainAppLayout() {
   const user = useLoaderData() as User | null;
 
   return (
-    <SidebarProvider defaultOpen={true}>
+    <SidebarProvider>
       {user && <AppSidebar user={user} />}
 
-      <SidebarInset className="overflow-hidden  flex flex-1 flex-col overflow-y-auto">
+      <SidebarInset className="overflow-hidden  flex flex-1 flex-col overflow-y-auto h-screen max-h-[calc(100vh-15px)]">
         <FinishOrgSetupBanner />
-
         {!user && (
           <div className="absolute top-4 right-4 z-10">
             <LoginButtons />
           </div>
         )}
-
         <Outlet />
       </SidebarInset>
     </SidebarProvider>
