960558271a1f079188f6804b365a0571cd6e88e1
Anthony DeMattos <demattosanthony@gmail.com>
2025-04-26 20:26:09 -0400

    fixed save sidebar state

--- web-app/src/components/layouts/main-app-layout.tsx
+++ web-app/src/components/layouts/main-app-layout.tsx
@@ -4,12 +4,15 @@ import { FinishOrgSetupBanner } from "@/features/organizations/components";
 import { AppSidebar } from "@/components/sidebar/app-sidebar";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 import { User } from "@/types/user";
+import { useCookies } from "react-cookie";
 
 export default function MainAppLayout() {
   const user = useLoaderData() as User | null;
+  const [cookies] = useCookies(["sidebar_state"]);
+  const defaultOpen = cookies.sidebar_state;
 
   return (
-    <SidebarProvider>
+    <SidebarProvider defaultOpen={defaultOpen}>
       {user && <AppSidebar user={user} />}
 
       <SidebarInset className="overflow-hidden  flex flex-1 flex-col overflow-y-auto h-screen max-h-[calc(100vh-15px)]">
